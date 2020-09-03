6178e042dea3d1f543311d58e5e17c873cd53d2f
peter <pkh879@gmail.com>
2020-09-03 18:04:57 +0900

    Create project to git repository

---
+++ src/main/java/com/example/awscloudstorage/AwscloudstorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscloudstorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwscloudstorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awscloudstorage/AwscloudstorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awscloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwscloudstorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
